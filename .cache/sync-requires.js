d4bc7edd2efc7beb2339675a0a48f3c1ede671ad
axbwh <30453713+axbwh@users.noreply.github.com>
2020-03-14 18:01:56 +1300

    nocache nopublic

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-project-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Google Drive\\Design\\Portfolio\\bannwarth\\src\\templates\\project.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Google Drive\\Design\\Portfolio\\bannwarth\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Google Drive\\Design\\Portfolio\\bannwarth\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Google Drive\\Design\\Portfolio\\bannwarth\\src\\pages\\about.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Google Drive\\Design\\Portfolio\\bannwarth\\src\\pages\\index.js")))
+  "component---src-templates-project-js": hot(preferDefault(require("C:\\Users\\bannw\\Desktop\\bannwarth\\src\\templates\\project.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\bannw\\Desktop\\bannwarth\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\bannw\\Desktop\\bannwarth\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\bannw\\Desktop\\bannwarth\\src\\pages\\about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\bannw\\Desktop\\bannwarth\\src\\pages\\index.js")))
 }
 
